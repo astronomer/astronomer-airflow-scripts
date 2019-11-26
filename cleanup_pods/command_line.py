b852045a7b5ee0d49859bc651b9d8574825e1bb0
Andrii Soldatenko <andrii.soldatenko@gmail.com>
2019-11-26 14:16:25 +0200

    added more tests

--- cleanup_pods/command_line.py
+++ cleanup_pods/command_line.py
@@ -1,24 +1,30 @@
 import argparse
 from kubernetes import client, config
+from kubernetes.client.rest import ApiException
 import logging
 
 # https://kubernetes.io/docs/concepts/workloads/pods/pod-lifecycle/
-POD_PHASE = [
-    # All Containers in the Pod have terminated in success, and will not be restarted.
-    'succeeded',
-    # All Containers in the Pod have terminated, and at least one Container has terminated in failure.
-    # That is, the Container either exited with non-zero status or was terminated by the system.
-    'failed'
-]
+# All Containers in the Pod have terminated in success, and will not be restarted.
+POD_SUCCEEDED = 'succeeded'
+
+# All Containers in the Pod have terminated, and at least one Container has terminated in failure.
+# That is, the Container either exited with non-zero status or was terminated by the system.
+POD_FAILED = 'failed'
 
 # https://kubernetes.io/docs/tasks/administer-cluster/out-of-resource/
-POD_REASON = ['evicted']
+POD_REASON_EVICTED = 'evicted'
+# If pod is failed and
+# restartPolicy is:
+# * Always: Restart Container; Pod phase stays Running.
+# * OnFailure: Restart Container; Pod phase stays Running.
+# * Never: Pod phase becomes Failed.
+POD_RESTART_POLICY_NEVER = 'never'
 
 
 def delete_pod(name, namespace):
     core_v1 = client.CoreV1Api()
     delete_options = client.V1DeleteOptions()
-    logging.warning('Deleting POD {name} from {namespace} namespace'.format(
+    logging.warning('Deleting POD "{name}" from "{namespace}" namespace'.format(
         name=name, namespace=namespace
     ))
     api_response = core_v1.delete_namespaced_pod(
@@ -28,15 +34,28 @@ def delete_pod(name, namespace):
     logging.warning(api_response)
 
 
-def cleanup(args):
+def cleanup(namespace):
     config.load_kube_config()
     core_v1 = client.CoreV1Api()
-    pod_list = core_v1.list_namespaced_pod(args.namespace)
+    pod_list = core_v1.list_namespaced_pod(namespace)
+
     for pod in pod_list.items:
-        pod_phase, pod_reason = pod.status.phase, pod.status.reason
-        if pod_phase.lower() in POD_PHASE or (pod_reason and pod_reason.lower() in POD_REASON):
-            logging.info('Pod phase {} and reason {}'.format(pod.status.phase, pod.status.reason))
-            delete_pod(pod.metadata.name, args.namespace)
+        pod_phase = pod.status.phase.lower()
+        pod_reason = pod.status.reason.lower() if pod.status.reason else ''
+        pod_restart_policy = pod.spec.restart_policy.lower()
+
+        if (pod_phase == POD_SUCCEEDED or
+           (pod_phase == POD_FAILED and pod_restart_policy == POD_RESTART_POLICY_NEVER) or
+           (pod_reason == POD_REASON_EVICTED)):
+
+            logging.info('Deleting pod "{}" phase "{}" and reason "{}", restart policy "{}"'.format(
+                pod.metadata.name, pod_phase, pod_reason, pod_restart_policy)
+            )
+            try:
+                delete_pod(pod.metadata.name, namespace)
+            except ApiException as e:
+                logging.error("can't remove POD: {}".format(e))
+                continue
 
 
 def main():
@@ -44,4 +63,4 @@ def main():
     parser.add_argument('--namespace', dest='namespace', default='default', type=str,
                         help='Namespace')
     args = parser.parse_args()
-    cleanup(args)
+    cleanup(args.namespace)
