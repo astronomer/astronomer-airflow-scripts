5c4ef2103ebcef7a3c93902582cc10d5e7c62ab2
Andrii Soldatenko <andrii.soldatenko@gmail.com>
2020-02-21 23:34:33 +0200

    fix more tests

--- tests/test_spinner.py
+++ tests/test_spinner.py
@@ -5,18 +5,18 @@ import pytest
 from astronomer.migration_spinner.command_line import spinner
 
 
-@mock.patch('alembic.runtime.migration.MigrationContext.get_current_revision')
-@mock.patch('alembic.script.base.ScriptDirectory.get_current_head')
-def test_spinner_success(get_current_head, get_current_revision):
-    get_current_revision.return_value = "00000000"
-    get_current_head.return_value = "00000000"
+@mock.patch('alembic.runtime.migration.MigrationContext.get_current_heads')
+@mock.patch('alembic.script.base.ScriptDirectory.get_heads')
+def test_spinner_success(get_heads, get_current_heads):
+    get_heads.return_value = ["00000000"]
+    get_current_heads.return_value = ["00000000"]
     spinner(timeout=0)
 
 
-@mock.patch('alembic.runtime.migration.MigrationContext.get_current_revision')
-@mock.patch('alembic.script.base.ScriptDirectory.get_current_head')
-def test_spinner_one_sec_timeout(get_current_head, get_current_revision):
-    get_current_revision.return_value = "00000000"
-    get_current_head.return_value = "10000000"
+@mock.patch('alembic.runtime.migration.MigrationContext.get_current_heads')
+@mock.patch('alembic.script.base.ScriptDirectory.get_heads')
+def test_spinner_one_sec_timeout(get_heads, get_current_heads):
+    get_current_heads.return_value = ["00000000"]
+    get_heads.return_value = ["10000000"]
     with pytest.raises(TimeoutError):
         spinner(timeout=1)
