da5c6c2fdb137540a55a182b45fe0985df682cd1
Andrii Soldatenko <andrii.soldatenko@gmail.com>
2020-02-24 14:05:43 +0200

    fix tests in master :( (#5)

--- tests/test_cleanup_pods.py
+++ tests/test_cleanup_pods.py
@@ -16,7 +16,8 @@ def test_delete_pod(delete_namespaced_pod):
 
 @mock.patch('astronomer.cleanup_pods.command_line.delete_pod')
 @mock.patch('kubernetes.client.CoreV1Api.list_namespaced_pod')
-def test_cleanup_succeeded_pods(list_namespaced_pod, delete_pod):
+@mock.patch('kubernetes.config.load_incluster_config')
+def test_cleanup_succeeded_pods(load_incluster_config, list_namespaced_pod, delete_pod):
     pod1 = MagicMock()
     pod1.metadata.name = 'dummy'
     pod1.status.phase = 'Succeeded'
@@ -24,11 +25,13 @@ def test_cleanup_succeeded_pods(list_namespaced_pod, delete_pod):
     list_namespaced_pod().items = [pod1]
     cleanup('awesome-namespace')
     delete_pod.assert_called_with('dummy', 'awesome-namespace')
+    load_incluster_config.assert_called_once()
 
 
 @mock.patch('astronomer.cleanup_pods.command_line.delete_pod')
 @mock.patch('kubernetes.client.CoreV1Api.list_namespaced_pod')
-def test_no_cleanup_failed_pods_wo_restart_policy_never(list_namespaced_pod, delete_pod):
+@mock.patch('kubernetes.config.load_incluster_config')
+def test_no_cleanup_failed_pods_wo_restart_policy_never(load_incluster_config, list_namespaced_pod, delete_pod):
     pod1 = MagicMock()
     pod1.metadata.name = 'dummy2'
     pod1.status.phase = 'Failed'
@@ -37,11 +40,13 @@ def test_no_cleanup_failed_pods_wo_restart_policy_never(list_namespaced_pod, del
     list_namespaced_pod().items = [pod1]
     cleanup('awesome-namespace')
     delete_pod.assert_not_called()
+    load_incluster_config.assert_called_once()
 
 
 @mock.patch('astronomer.cleanup_pods.command_line.delete_pod')
 @mock.patch('kubernetes.client.CoreV1Api.list_namespaced_pod')
-def test_cleanup_failed_pods_w_restart_policy_never(list_namespaced_pod, delete_pod):
+@mock.patch('kubernetes.config.load_incluster_config')
+def test_cleanup_failed_pods_w_restart_policy_never(load_incluster_config, list_namespaced_pod, delete_pod):
     pod1 = MagicMock()
     pod1.metadata.name = 'dummy3'
     pod1.status.phase = 'Failed'
@@ -50,11 +55,13 @@ def test_cleanup_failed_pods_w_restart_policy_never(list_namespaced_pod, delete_
     list_namespaced_pod().items = [pod1]
     cleanup('awesome-namespace')
     delete_pod.assert_called_with('dummy3', 'awesome-namespace')
+    load_incluster_config.assert_called_once()
 
 
 @mock.patch('astronomer.cleanup_pods.command_line.delete_pod')
 @mock.patch('kubernetes.client.CoreV1Api.list_namespaced_pod')
-def test_cleanup_evicted_pods(list_namespaced_pod, delete_pod):
+@mock.patch('kubernetes.config.load_incluster_config')
+def test_cleanup_evicted_pods(load_incluster_config, list_namespaced_pod, delete_pod):
     pod1 = MagicMock()
     pod1.metadata.name = 'dummy4'
     pod1.status.phase = 'Failed'
@@ -63,11 +70,13 @@ def test_cleanup_evicted_pods(list_namespaced_pod, delete_pod):
     list_namespaced_pod().items = [pod1]
     cleanup('awesome-namespace')
     delete_pod.assert_called_with('dummy4', 'awesome-namespace')
+    load_incluster_config.assert_called_once()
 
 
 @mock.patch('astronomer.cleanup_pods.command_line.delete_pod')
 @mock.patch('kubernetes.client.CoreV1Api.list_namespaced_pod')
-def test_cleanup_api_exception_continue(list_namespaced_pod, delete_pod):
+@mock.patch('kubernetes.config.load_incluster_config')
+def test_cleanup_api_exception_continue(load_incluster_config, list_namespaced_pod, delete_pod):
     delete_pod.side_effect = kubernetes.client.rest.ApiException(status=0)
     pod1 = MagicMock()
     pod1.metadata.name = 'dummy'
@@ -75,3 +84,4 @@ def test_cleanup_api_exception_continue(list_namespaced_pod, delete_pod):
     pod1.status.reason = None
     list_namespaced_pod().items = [pod1]
     cleanup('awesome-namespace')
+    load_incluster_config.assert_called_once()
