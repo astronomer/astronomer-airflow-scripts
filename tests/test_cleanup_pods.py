dba8210f71c739046a6589efdc83cbe2e43ff743
abhishekbhakat <abhishek.bhakat@hotmail.com>
2023-11-03 05:27:01 +0000

    test case for kpo pod deletetion

--- tests/test_cleanup_pods.py
+++ tests/test_cleanup_pods.py
@@ -109,3 +109,20 @@ def test_cleanup_kpo_pods_delay(load_incluster_config, list_namespaced_pod, dele
     cleanup('awesome-namespace')
     delete_pod.assert_not_called()
     load_incluster_config.assert_called_once()
+
+@mock.patch('astronomer.cleanup_pods.command_line.delete_pod')
+@mock.patch('kubernetes.client.CoreV1Api.list_namespaced_pod')
+@mock.patch('kubernetes.config.load_incluster_config')
+def test_cleanup_kpo_pods_delete_after_threshold(load_incluster_config, list_namespaced_pod, delete_pod):
+    pod1 = MagicMock()
+    pod1.metadata.name = 'dummy'
+    pod1.metadata.labels = {'airflow_kpo_in_cluster': True}
+    pod1.status.phase = 'Succeeded'
+    pod1.status.reason = None
+    container1 = MagicMock()
+    container1.last_transition_time = datetime.now(timezone.utc) - timedelta(seconds=4000)
+    pod1.status.conditions = [container1]
+    list_namespaced_pod().items = [pod1]
+    cleanup('awesome-namespace')
+    delete_pod.assert_called_once()
+    load_incluster_config.assert_called_once()
