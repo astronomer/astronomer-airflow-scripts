2678c6f3bb4c610fa1b711d80a35ce64e18fd9d4
Jed Cunningham <66968678+jedcunningham@users.noreply.github.com>
2025-12-02 08:41:49 -0700

    Fix handling for pods with no labels (#18)

--- tests/test_cleanup_pods.py
+++ tests/test_cleanup_pods.py
@@ -126,3 +126,17 @@ def test_cleanup_kpo_pods_delete_after_threshold(load_incluster_config, list_nam
     cleanup('awesome-namespace')
     delete_pod.assert_called_once()
     load_incluster_config.assert_called_once()
+
+@mock.patch('astronomer.cleanup_pods.command_line.delete_pod')
+@mock.patch('kubernetes.client.CoreV1Api.list_namespaced_pod')
+@mock.patch('kubernetes.config.load_incluster_config')
+def test_cleanup_no_labels(load_incluster_config, list_namespaced_pod, delete_pod):
+    pod1 = MagicMock()
+    pod1.metadata.name = 'dummy'
+    pod1.metadata.labels = None
+    pod1.status.phase = 'Succeeded'
+    pod1.status.reason = None
+    list_namespaced_pod().items = [pod1]
+    cleanup('awesome-namespace')
+    delete_pod.assert_called_with('dummy', 'awesome-namespace')
+    load_incluster_config.assert_called_once()
